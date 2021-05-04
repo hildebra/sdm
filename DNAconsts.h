c5032b1bcd302936ff45575f433d9570915ae513
Falk Hildebrand (QIB) <hildebra@v0768.nbi.ac.uk>
2021-05-04 14:05:19 +0100

    release

--- DNAconsts.h
+++ DNAconsts.h
@@ -103,7 +103,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif // _WIN32
 
 
-static const float sdm_version = 1.82f;
+static const float sdm_version = 1.83f;
 static const char* sdm_status = "beta";
 
 
